a230096f65906b5744c6c2e18bbad957b691e432
Mateusz Mikołaj Okulus <m.okulus@stud.elka.pw.edu.pl>
2022-01-17 13:13:36 +0100

    Also filter aliens in AlienSystem._alien_iter

--- space_invaders/alien_system.py
+++ space_invaders/alien_system.py
@@ -22,7 +22,7 @@ class AlienSystem(system.System):
             for x in range(11):
                 alien = Alien(self._game, Point(x, y))
                 self._aliens.append(alien)
-        self._alien_iter = iter(self._aliens)
+        self._alien_iter = self._make_alien_iter()
         self._initialized = False
 
     def tick(self):
@@ -41,15 +41,21 @@ class AlienSystem(system.System):
         if not self._initialized:
             self._init_animation()
             return
-        next_alien = next(self._alien_iter, None)
-        if next_alien:
-            next_alien.move(self._velocity)
+        alien = next(self._alien_iter, None)
+        if alien:
+            alien.move(self._velocity)
         else:
-            self._alien_iter = iter(self._aliens)
-            aliens_max_pos_x = min((alien.position().x for alien in self._aliens))
-            aliens_min_pos_x = max((alien.position().x for alien in self._aliens))
+            self._alien_iter = self._make_alien_iter()
+            aliens_max_pos_x = min(
+                (alien.position().x for alien in self._aliens)
+            )
+            aliens_min_pos_x = max(
+                (alien.position().x for alien in self._aliens)
+            )
             min_x_allowed = 0
-            max_x_allowed = (self._game.settings.width() - assets.aliens()[0][0].shape[0])
+            max_x_allowed = (
+                self._game.settings.width() - assets.aliens()[0][0].shape[0]
+            )
             at_left_edge = aliens_min_pos_x == min_x_allowed
             at_right_edge = aliens_max_pos_x == max_x_allowed
             if at_left_edge or at_right_edge:
@@ -66,12 +72,15 @@ class AlienSystem(system.System):
         alien = next(self._alien_iter, None)
         if not alien:
             self._initialized = True
-            self._alien_iter = iter(self._aliens)
+            self._alien_iter = self._make_alien_iter()
             self._game.spawn(self._game.player)
             self._game.add_system(BulletSystem(self._game, self))
             self._game.add_system(SaucerSystem(self._game))
         else:
             self._game.spawn(alien)
+
+    def _make_alien_iter(self):
+        return iter(alien for alien in self._aliens if alien.alive)
 
     def aliens(self):
         """
